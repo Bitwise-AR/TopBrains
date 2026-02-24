f328b3670f9d4b16251f7e12768218725b13f054
bitwise-ar <rajayush1412@gmail.com>
2026-02-24 12:38:46 +0530

    updates

---
+++ AdoNet and LINQ/LINQ/Linq2/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // return duplicate products
+        string input = Console.ReadLine();
+        List<string> products = input.Split(' ').ToList();
+        List<string> uniqueProducts = products.Distinct().ToList();
+        Console.WriteLine("Duplicate products: ");
+        foreach (string product in uniqueProducts)
+        {
+            int count = products.Count(p => p == product);
+            if (count > 1)
+            {
+                Console.WriteLine(product );
+            }
+        }
+    }
+}
